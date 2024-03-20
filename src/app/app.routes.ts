ac52a26f8555a6b2f33a7345c98901be4d46b33a
kenneth luzolo <kenneth.nepsther@gmail.com>
2024-03-20 23:35:12 +0100

    :tada: Commit inicial

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,5 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path:'budget-planner',loadChildren:()=> import('./budget-planner/budget-planner-routing.module').then(m => m.BudgetPlannerRoutingModule)}
+];
